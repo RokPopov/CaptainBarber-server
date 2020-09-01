8fcdf31e255bb6276a48ceb2bff40477f893d5ab
rokpopov <rokpopov90@gmail.com>
2020-09-01 18:49:08 +0200

    Set up reservation model and migration

---
+++ models/reservation.js
@@ -0,0 +1,38 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class reservation extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  reservation.init(
+    {
+      time: {
+        type: DataTypes.DATE,
+      },
+      haircut: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      beardcut: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      combo: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "reservation",
+    }
+  );
+  return reservation;
+};
