58667aed55978133b4f080df1406c7365fd67ce7
rokpopov <rokpopov90@gmail.com>
2020-09-01 18:30:20 +0200

    Set up barbershop model and migration

---
+++ models/barbershop.js
@@ -0,0 +1,71 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class barbershop extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  barbershop.init(
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      haircut: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      beardcut: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      combo: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      haircutPrice: {
+        type: DataTypes.INTEGER,
+      },
+      beardcutPrice: {
+        type: DataTypes.INTEGER,
+      },
+      comboPrice: {
+        type: DataTypes.INTEGER,
+      },
+      website: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+      },
+      phoneNum: {
+        type: DataTypes.INTEGER,
+      },
+      openingHours: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+      },
+      image: {
+        type: DataTypes.TEXT,
+      },
+      rate: {
+        type: DataTypes.INTEGER,
+      },
+    },
+    {
+      sequelize,
+      modelName: "barbershop",
+    }
+  );
+  return barbershop;
+};
