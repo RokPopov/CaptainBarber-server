c13bc4570addb3158faa08ea00ef10b6fd5c87e6
rokpopov <rokpopov90@gmail.com>
2020-09-01 17:24:17 +0200

    Set up sequelize

---
+++ routers/auth.js
@@ -0,0 +1,19 @@
+const bcrypt = require("bcrypt");
+const { Router } = require("express");
+const { toJWT } = require("../auth/jwt");
+const authMiddleware = require("../auth/middleware");
+const User = require("../models/").user;
+const { SALT_ROUNDS } = require("../config/constants");
+
+const router = new Router();
+
+// The /me endpoint can be used to:
+// - get the users email & name using only their token
+// - checking if a token is (still) valid
+router.get("/me", authMiddleware, async (req, res) => {
+  // don't send back the password hash
+  delete req.user.dataValues["password"];
+  res.status(200).send({ ...req.user.dataValues });
+});
+
+module.exports = router;
