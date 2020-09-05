5783a18c417575d0e90d95346b5ecdb317c35b83
rokpopov <rokpopov90@gmail.com>
2020-09-05 14:33:31 +0200

    Add login endpoint

--- routers/auth.js
+++ routers/auth.js
@@ -2,10 +2,39 @@ const bcrypt = require("bcrypt");
 const { Router } = require("express");
 const { toJWT } = require("../auth/jwt");
 const authMiddleware = require("../auth/middleware");
-//const User = require("../models/").user;
 const { SALT_ROUNDS } = require("../config/constants");
 
+const User = require("../models/").user;
+
 const router = new Router();
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    console.log("what are you sending", req.body);
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .send({ message: "Please provide both email and password" });
+    }
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      return res.status(400).send({
+        message: "User with that email not found or password incorrect",
+      });
+    }
+
+    delete user.dataValues["password"];
+    const token = toJWT({ userId: user.id });
+    return res.status(200).send({ token, ...user.dataValues });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({ message: "Something went wrong, sorry" });
+  }
+});
 
 // The /me endpoint can be used to:
 // - get the users email & name using only their token
